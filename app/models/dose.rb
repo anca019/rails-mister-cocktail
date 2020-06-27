d65b60b498a548482d4071daa6602eb6e0c9419b
ancastan <ankastan@ymail.com>
2020-06-27 14:09:47 +0200

    Routes created + Cocktails controller actions created

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,5 +3,6 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
   validates :description, presence: true
   validates :cocktail, presence: true
-  validates :ingredient, presence: true
+  validates :ingredient, presence: true, uniqueness: { scope: [:cocktail],
+    message: "There should be a unique dose for a given cocktail/ingredient couple" }
 end
