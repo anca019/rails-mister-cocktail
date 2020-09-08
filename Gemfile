5483368f92294c544cf8dfdeda6b5a03909968ae
ancastan <ankastan@ymail.com>
2020-09-08 19:44:26 +0200

    pg_search gem installed

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'font-awesome-rails', '~> 4.4'
 gem 'simple_form'
 gem 'dotenv-rails', groups: [:development, :test]
 gem 'cloudinary', '~> 1.12.0'
+gem 'pg_search', '~> 2.3.0'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,4 +3,13 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   has_one_attached :photo
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [ :name ],
+    associated_against: {
+      ingredient: [ :name ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
