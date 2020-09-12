c1cac2c3ef1d89474d1cf54cb2ca21eb74d6b83a
ancastan <ankastan@ymail.com>
2020-09-12 11:05:48 +0200

    pg_search implemented + homepage created

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,6 +1,10 @@
 class CocktailsController < ApplicationController
   def index
-    @cocktails = Cocktail.all
+    if params[:query].present?
+      @cocktails = Cocktail.search_by_name_and_ingredient(params[:query])
+    else
+      @cocktails = Cocktail.all
+    end
   end
 
   def show
--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,13 +3,14 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   has_one_attached :photo
+
   include PgSearch::Model
-  pg_search_scope :global_search,
-    against: [ :name ],
-    associated_against: {
-      ingredient: [ :name ]
-    },
-    using: {
-      tsearch: { prefix: true }
-    }
+    pg_search_scope :search_by_name_and_ingredient,
+      against: [ :name ],
+      associated_against: {
+        ingredients: [ :name ]
+      },
+      using: {
+        tsearch: { prefix: true }
+      }
 end
